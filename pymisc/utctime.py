5b0acd915c0c839a870f4752ad6deb41d9677c50
tacchangxyz <user001@tacchang.xyz>
2019-03-23 13:37:15 +0900

    基本はできた。

--- pymisc/utctime.py
+++ pymisc/utctime.py
@@ -1,17 +1,120 @@
+"""
+Python公式ドキュメントより抜粋
+https://www.soudegesu.com/python/python-datetime/
+
+aware オブジェクト
+aware オブジェクトは他の aware オブジェクトとの相対関係を把握出来るように、
+タイムゾーンや夏時間の情報のような、アルゴリズム的で政治的な適用可能な
+時間調節に関する知識を持っています。 aware オブジェクトは解釈の余地のない
+特定の実時刻を表現するのに利用されます。
+
+naive オブジェクト
+naive オブジェクトには他の日付時刻オブジェクトとの相対関係を把握するのに
+足る情報が含まれません。 あるプログラム内の数字がメートルを表わしているのか、
+マイルなのか、それとも質量なのかがプログラムによって異なるように、
+naive オブジェクトが協定世界時 (UTC) なのか、現地時間なのか、それとも
+他のタイムゾーンなのかはそのプログラムに依存します。 Naive オブジェクトは
+いくつかの現実的な側面を無視してしまうというコストを無視すれば、簡単に
+理解でき、うまく利用することができます。
+"""
 from datetime import datetime
-from pytz import timezone, utc
-from tzlocal import get_localzone
+import pytz
+# この書き方だとpatchできない
+# from tzlocal import get_localzone
+import tzlocal
 
-_NATIVE_TIMEZONE = get_localzone()
+"""
+タイムゾーン
+https://jp.cybozu.help/general/ja/admin/list_systemadmin/list_system_time/timezone.html
+"""
+
+"""
+出典
+https://narito.ninja/blog/detail/81/
+https://www.soudegesu.com/python/python-datetime/
+https://note.nkmk.me/python-datetime-isoformat-fromisoformat/
+https://qiita.com/shota243/items/91660ece72b5e84c3adb
+"""
+
+"""
+キーワード
+ISO8601, RFC3339
+"""
 
 
-def naive_to_utc(native_datetime):
-    return get_localzone().localize(native_datetime).astimezone(utc)
+class AwareDatetime:
+    def __init__(self, naive_time, tz=None):
+        """
+        指定されたローカル時間からUTCやローカル文字列など生成する。
+
+        :param naive_time: naive時間。tzが設定されていても無視する。
+        :param tz: タイムゾーン。省略された場合はPCに設定されているタイムゾーン。
+        """
+        self._tz = tz
+        if self._tz is None:
+            self._tz = tzlocal.get_localzone()
+        _dt = datetime(
+            year=naive_time.year,
+            month=naive_time.month,
+            day=naive_time.day,
+            hour=naive_time.hour,
+            minute=naive_time.minute,
+            second=naive_time.second,
+            microsecond=naive_time.microsecond)
+        if self._tz is not pytz.utc:
+            self._utc = self._tz.localize(_dt).astimezone(pytz.utc)
+        else:
+            # 内部ではUTCで保持する
+            self._utc = pytz.utc.localize(_dt)
+
+    @property
+    def datetime(self):
+        return self._utc
+
+    @property
+    def local_datetime(self):
+        return self._utc.astimezone(tzlocal.get_localzone())
+
+    def to_local_string(self, form='%Y/%m/%d %H:%M.%S'):
+        return self._utc.astimezone(tzlocal.get_localzone()).strftime(form)
+
+    def to_iso_string(self):
+        return self._utc.isoformat()
+
+    def to_local_iso_string(self):
+        return self._utc.astimezone(tzlocal.get_localzone()).isoformat()
+        # _naive = self._utc.astimezone(tzlocal.get_localzone()).replace(tzinfo=None)
+        # return self._tz.normalize(self._tz.localize(_naive)).isoformat()
+
+    def __str__(self):
+        return self._utc
 
 
-def local_to_utc(local_datetime):
-    return local_datetime.replace(tzinfo=utc)
-
-
-def utc_to_local(utc_datetime):
-    return _NATIVE_TIMEZONE.localize(utc_datetime.replace(tzinfo=None))
+if __name__ == '__main__':
+    _now = datetime.now()
+    print(_now)
+    print(_now.tzinfo)
+    print("localize UTC: {}".format(pytz.utc.localize(_now)))
+    print("localize JST: {}".format(tzlocal.get_localzone().localize(_now)))
+    print("replace: {}".format(_now.replace(tzinfo=tzlocal.get_localzone())))   # +9:19
+    print("")
+    aware = AwareDatetime(_now)
+    print("datetime: {}".format(aware.datetime))
+    print("local_datetime: {}".format(aware.local_datetime))
+    print("to_local_string: {}".format(aware.to_local_string()))
+    print("to_iso_string: {}".format(aware.to_iso_string()))
+    print("to_local_iso_string: {}".format(aware.to_local_iso_string()))
+    print("")
+    aware = AwareDatetime(_now, tzlocal.get_localzone())
+    print("datetime: {}".format(aware.datetime))
+    print("local_datetime: {}".format(aware.local_datetime))
+    print("to_local_string: {}".format(aware.to_local_string()))
+    print("to_iso_string: {}".format(aware.to_iso_string()))
+    print("to_local_iso_string: {}".format(aware.to_local_iso_string()))
+    print("")
+    aware = AwareDatetime(_now, pytz.utc)
+    print("datetime: {}".format(aware.datetime))
+    print("local_datetime: {}".format(aware.local_datetime))
+    print("to_local_string: {}".format(aware.to_local_string()))
+    print("to_iso_string: {}".format(aware.to_iso_string()))
+    print("to_local_iso_string: {}".format(aware.to_local_iso_string()))
--- tests/test_utctime.py
+++ tests/test_utctime.py
@@ -1,22 +1,49 @@
 from unittest import TestCase
+from unittest.mock import Mock, patch
 
 from datetime import datetime, timezone, timedelta
 import pytz
-from tzlocal import get_localzone
+# この書き方だとpatchできない
+# from tzlocal import get_localzone
+import tzlocal
 
-from pymisc.utctime import utc_to_local, local_to_utc
+from pymisc.utctime import AwareDatetime
+
+_UTC_NAIVE_DATETIME = datetime(2018, 12, 31, 5, 0, 30, 1000)
+_UTC_AWARE_DATETIME = datetime(2018, 12, 31, 5, 0, 30, 1000, pytz.utc)
+_UTC_DATETIME_STRING = '2018-12-31 05:00:30.001000+00:00'
+_CST_ID = 'Asia/Shanghai'
+_CST_NAIVE_DATETIME = datetime(2018, 12, 31, 13, 0, 30, 1000)
+_CST_STRING = '2018-12-31 13:00:30.001000+08:00'
+_CST_ISO_STRING = '2018-12-31T13:00:30.001000+08:00'
+_CST_DATETIME_STRING = '2018/12/31 13:00.30'
+_JST_ID = 'Asia/Tokyo'
+_JST_NAIVE_DATETIME = datetime(2018, 12, 31, 14, 0, 30, 1000)
+_JST_STRING = '2018-12-31 14:00:30.001000+09:00'
+_JST_ISO_STRING = '2018-12-31T14:00:30.001000+09:00'
+_JST_DATETIME_STRING = '2018/12/31 14:00.30'
+
+
+def timezone_china():
+    return pytz.timezone(_CST_ID)
 
 
 class TestUtctime(TestCase):
 
-    def test_native_to_utc(self):
-        dt_local = datetime(2018, 12, 31, 5, 0, 30, 1000)
-        dt_local = get_localzone().localize(dt_local.replace(tzinfo=None))
-        expect = '2018-12-31 05:00:30.001000+00:00'
-        self.assertEqual(expect, str(local_to_utc(dt_local)))
+    @patch('tzlocal.get_localzone', timezone_china)
+    def test_to_local_string(self):
+        expect = _CST_DATETIME_STRING
+        aware = AwareDatetime(_CST_NAIVE_DATETIME, pytz.timezone(_CST_ID))
+        self.assertEqual(expect, str(aware.to_local_string()))
 
-    def test_utc_to_native(self):
-        dt_utc = datetime(2018, 12, 31, 5, 0, 30, 1000, tzinfo=timezone.utc)
-        expect = '2018-12-31 05:00:30.001000+09:00'
-        self.assertEqual(expect, str(utc_to_local(dt_utc)))
+    @patch('tzlocal.get_localzone', timezone_china)
+    def test_datetime(self):
+        _dt = AwareDatetime(_UTC_NAIVE_DATETIME, pytz.utc)
+        expect = _UTC_DATETIME_STRING
+        self.assertEqual(expect, str(_dt.datetime))
 
+    @patch('tzlocal.get_localzone', timezone_china)
+    def test_local_datetime(self):
+        _dt = AwareDatetime(_CST_NAIVE_DATETIME)
+        expect = _CST_STRING
+        self.assertEqual(expect, str(_dt.local_datetime))
